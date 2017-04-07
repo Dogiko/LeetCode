58ae65fe6c2cf8e645c0de4d29beeb07c654466c
Wen-yen Feng <dogikomk2@gmail.com>
2017-04-08 00:13:23 +0800

    algorithms 58 solved

---
+++ Algorithms/058. Length of Last Word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        temp = s.strip().split(" ")
+        return len(temp[-1])
